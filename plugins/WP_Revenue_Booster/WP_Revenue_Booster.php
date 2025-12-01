fe584950b21a694d1bb1b5c1257579da98fc334d
Naveen Kumar <109812484+naveengoyal1503@users.noreply.github.com>
2025-12-01 16:18:32 +0530

    Add plugin WP_Revenue_Booster

--- plugins/WP_Revenue_Booster/WP_Revenue_Booster.php
+++ plugins/WP_Revenue_Booster/WP_Revenue_Booster.php
@@ -5,18 +5,42 @@ Author URI: https://automation.bhandarum.in/generated-plugins/tracker.php?plugin
 <?php
 /**
  * Plugin Name: WP Revenue Booster
- * Description: Automatically optimizes ad, affiliate, and membership revenue by analyzing visitor behavior.
+ * Plugin URI: https://example.com/wp-revenue-booster
+ * Description: Automatically optimizes ad placement, affiliate links, and upsell offers to maximize site revenue.
  * Version: 1.0
- * Author: Cozmo Labs
+ * Author: Revenue Labs
+ * Author URI: https://example.com
+ * License: GPL2
  */
 
 class WP_Revenue_Booster {
 
     public function __construct() {
+        add_action('wp_head', array($this, 'add_tracking_code'));
+        add_action('the_content', array($this, 'inject_optimized_content'));
         add_action('admin_menu', array($this, 'add_admin_menu'));
-        add_action('wp_footer', array($this, 'inject_monetization_elements'));
-        add_action('wp_ajax_wprb_save_settings', array($this, 'save_settings'));
-        add_action('wp_ajax_nopriv_wprb_save_settings', array($this, 'save_settings'));
+        add_action('admin_init', array($this, 'settings_init'));
+    }
+
+    public function add_tracking_code() {
+        echo '<script>console.log("WP Revenue Booster: Tracking active");</script>';
+    }
+
+    public function inject_optimized_content($content) {
+        $settings = get_option('wp_revenue_booster_settings');
+        $ad_code = isset($settings['ad_code']) ? $settings['ad_code'] : '';
+        $affiliate_link = isset($settings['affiliate_link']) ? $settings['affiliate_link'] : '';
+
+        if (is_single() && !empty($ad_code)) {
+            $content = str_replace('</p>', '</p>' . $ad_code, $content, 1);
+        }
+
+        if (is_single() && !empty($affiliate_link)) {
+            $upsell = '<p><a href="' . esc_url($affiliate_link) . '" target="_blank">Check out this recommended product!</a></p>';
+            $content .= $upsell;
+        }
+
+        return $content;
     }
 
     public function add_admin_menu() {
@@ -24,78 +48,63 @@ class WP_Revenue_Booster {
             'WP Revenue Booster',
             'Revenue Booster',
             'manage_options',
-            'wp-revenue-booster',
-            array($this, 'settings_page')
+            'wp_revenue_booster',
+            array($this, 'options_page')
         );
     }
 
-    public function settings_page() {
-        if (!current_user_can('manage_options')) return;
-        $settings = get_option('wprb_settings', array());
+    public function settings_init() {
+        register_setting('wpRevenueBooster', 'wp_revenue_booster_settings');
+
+        add_settings_section(
+            'wp_revenue_booster_section',
+            'Revenue Booster Settings',
+            null,
+            'wpRevenueBooster'
+        );
+
+        add_settings_field(
+            'ad_code',
+            'Ad Code',
+            array($this, 'ad_code_render'),
+            'wpRevenueBooster',
+            'wp_revenue_booster_section'
+        );
+
+        add_settings_field(
+            'affiliate_link',
+            'Affiliate Link',
+            array($this, 'affiliate_link_render'),
+            'wpRevenueBooster',
+            'wp_revenue_booster_section'
+        );
+    }
+
+    public function ad_code_render() {
+        $settings = get_option('wp_revenue_booster_settings');
+        $value = isset($settings['ad_code']) ? $settings['ad_code'] : '';
+        echo '<textarea name="wp_revenue_booster_settings[ad_code]" rows="5" cols="50">' . esc_textarea($value) . '</textarea>';
+    }
+
+    public function affiliate_link_render() {
+        $settings = get_option('wp_revenue_booster_settings');
+        $value = isset($settings['affiliate_link']) ? $settings['affiliate_link'] : '';
+        echo '<input type="text" name="wp_revenue_booster_settings[affiliate_link]" value="' . esc_attr($value) . '" size="50" />';
+    }
+
+    public function options_page() {
         ?>
         <div class="wrap">
             <h1>WP Revenue Booster</h1>
-            <form method="post" action="javascript:void(0);" id="wprb-settings-form">
-                <table class="form-table">
-                    <tr>
-                        <th><label>Ad Code</label></th>
-                        <td><textarea name="ad_code" rows="5" cols="50"><?php echo esc_textarea($settings['ad_code'] ?? ''); ?></textarea></td>
-                    </tr>
-                    <tr>
-                        <th><label>Affiliate Link</label></th>
-                        <td><input type="text" name="affiliate_link" value="<?php echo esc_attr($settings['affiliate_link'] ?? ''); ?>" size="50" /></td>
-                    </tr>
-                    <tr>
-                        <th><label>Membership CTA</label></th>
-                        <td><input type="text" name="membership_cta" value="<?php echo esc_attr($settings['membership_cta'] ?? ''); ?>" size="50" /></td>
-                    </tr>
-                </table>
-                <button type="submit" class="button button-primary">Save Settings</button>
+            <form action="options.php" method="post">
+                <?php
+                settings_fields('wpRevenueBooster');
+                do_settings_sections('wpRevenueBooster');
+                submit_button();
+                ?>
             </form>
-            <div id="wprb-message"></div>
         </div>
-        <script>
-            jQuery('#wprb-settings-form').on('submit', function() {
-                jQuery.post(ajaxurl, {
-                    action: 'wprb_save_settings',
-                    ad_code: jQuery('[name=ad_code]').val(),
-                    affiliate_link: jQuery('[name=affiliate_link]').val(),
-                    membership_cta: jQuery('[name=membership_cta]').val(),
-                    security: '<?php echo wp_create_nonce('wprb_save_settings'); ?>'
-                }, function(response) {
-                    jQuery('#wprb-message').html('<p>Settings saved!</p>');
-                });
-            });
-        </script>
         <?php
-    }
-
-    public function save_settings() {
-        check_ajax_referer('wprb_save_settings', 'security');
-        $settings = array(
-            'ad_code' => sanitize_textarea_field($_POST['ad_code']),
-            'affiliate_link' => esc_url_raw($_POST['affiliate_link']),
-            'membership_cta' => sanitize_text_field($_POST['membership_cta'])
-        );
-        update_option('wprb_settings', $settings);
-        wp_die('Settings saved.');
-    }
-
-    public function inject_monetization_elements() {
-        $settings = get_option('wprb_settings', array());
-        if (empty($settings)) return;
-
-        // Simple logic: show ad if user is new, affiliate if returning, CTA if engaged
-        $is_returning = isset($_COOKIE['wprb_returning']) ? true : false;
-        setcookie('wprb_returning', 1, time() + 3600 * 24 * 30, '/');
-
-        if (!$is_returning && !empty($settings['ad_code'])) {
-            echo '<div class="wprb-ad">' . $settings['ad_code'] . '</div>';
-        } elseif (!empty($settings['affiliate_link'])) {
-            echo '<div class="wprb-affiliate"><a href="' . $settings['affiliate_link'] . '" target="_blank">Check out this deal!</a></div>';
-        } elseif (!empty($settings['membership_cta'])) {
-            echo '<div class="wprb-membership"><a href="#">' . $settings['membership_cta'] . '</a></div>';
-        }
     }
 }
 
